320f91338827b777e68400456b94697d1c29c180
unyon <michalloran@hotmail.com>
2016-01-21 20:44:54 +0000

    commit for test review

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+   quotes = Quote.create([{ saying: 'Chicago' }, { author: 'Mike Mike' }])
+   
